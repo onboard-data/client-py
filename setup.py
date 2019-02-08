32d0c59471f6e88d4fba75cfef9951890eb4dd7e
luke <luke.walsh@onboarddata.io>
2019-02-08 13:36:58 -0500

    added setup and init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='api_sdk',
+      version='0.1',
+      description='Onboard API SDK',
+      author='Luke Walsh',
+      author_email='luke.walsh@onboarddata.io',
+      url='https://www.onboarddata.io',
+      packages=['api_sdk'],
+     )
