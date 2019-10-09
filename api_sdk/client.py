ddb8f93574a4d4b78320c596b22f524454841af2
Nathan Merritt <gnm@the-merritts.net>
2019-10-09 11:21:48 -0600

    dry up the auth code a bit

--- api_sdk/client.py
+++ api_sdk/client.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import urllib.parse
 
 
@@ -19,14 +18,19 @@ class APIClient:
                 'password': self._pw,
             }
             headers = { 'Content-Type': 'application/json'}
-            response = requests.post(url, data=json.dumps(payload), headers=headers)
+            response = requests.post(url, json=payload, headers=headers)
             self._token = response.json()['userInfo']['token']
         return self._token
 
-    def get_all_points(self):
+    def auth(self):
         token = self._get_token()
+        return {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'}
+
+    def get_all_points(self):
+        headers = self.auth()
         buildings_url = '{}/buildings'.format(self._api_url)
-        headers = {'Authorization': 'Bearer {}'.format(token)}
         buildings = requests.get(buildings_url, headers=headers).json()
         bldg_ids = list(map(lambda b: b['id'], buildings))
         point_ids = []
@@ -43,47 +47,37 @@ class APIClient:
             yield l[i:i + n]
 
     def get_points_by_ids(self, point_ids):
-        token = self._get_token()
         point_ids_chunked = list(self.divide_chunks(point_ids, 500))
         points = []
         for chunk in point_ids_chunked:
             points_str = '[' + ','.join(str(id) for id in chunk) + ']'
             url = '{}/points?point_ids={}'.format(self._api_url, points_str)
-            headers = {'Authorization': 'Bearer {}'.format(token)}
-            points_chunk = requests.get(url, headers=headers).json()
+            points_chunk = requests.get(url, headers=self.auth()).json()
             points += points_chunk
         return points
 
     def get_points_by_datasource(self, datasource_hashes):
-        token = self._get_token()
         datasource_hashes_chunked = list(self.divide_chunks(datasource_hashes, 125))
         points = []
         for chunk in datasource_hashes_chunked:
             hashes_str = "[" + ','.join([r"'" + c + r"'" for c in chunk]) + "]"
             query = urllib.parse.quote(hashes_str)
             url = '{}/points?datasource_hashes={}'.format(self._api_url, query)
-            headers = {'Authorization': 'Bearer {}'.format(token)}
-            points_chunk = requests.get(url, headers=headers).json()
+            points_chunk = requests.get(url, headers=self.auth()).json()
             points += points_chunk
         return points
 
     def get_all_point_types(self):
-        token = self._get_token()
         url = '{}/pointtypes'.format(self._api_url)
-        headers = {'Authorization': 'Bearer {}'.format(token)}
-        point_types = requests.get(url, headers=headers).json()
+        point_types = requests.get(url, headers=self.auth()).json()
         return point_types
 
     def get_all_measurements(self):
-        token = self._get_token()
         url = '{}/measurements'.format(self._api_url)
-        headers = {'Authorization': 'Bearer {}'.format(token)}
-        measurements = requests.get(url, headers=headers).json()
+        measurements = requests.get(url, headers=self.auth()).json()
         return measurements
 
     def get_all_units(self):
-        token = self._get_token()
         url = '{}/unit'.format(self._api_url)
-        headers = {'Authorization': 'Bearer {}'.format(token)}
-        units = requests.get(url, headers=headers).json()
+        units = requests.get(url, headers=self.auth()).json()
         return units
