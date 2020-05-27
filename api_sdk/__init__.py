91688dd99ef262647f532001a0fd3113cdb84daf
Nathan Merritt <gnm@the-merritts.net>
2020-05-27 11:33:12 -0600

    top-level export of OnboardClient

--- api_sdk/__init__.py
+++ api_sdk/__init__.py
@@ -1,2 +1,4 @@
 from .client import APIClient, ProductionAPIClient, DevelopmentAPIClient  # noqa: F401
 from .exceptions import OnboardApiException, OnboardTemporaryException  # noqa: F401
+
+OnboardClient = ProductionAPIClient
