4734898295e487cb8d205e003765cac2dba34d5c
Nathan Merritt <nathan.merritt@onboarddata.io>
2020-06-23 15:08:05 -0600

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.0.0',
+      version='1.1.0',
       author='Nathan Merritt',
       author_email='nathan.merritt@onboarddata.io',
       description='Onboard API SDK',
