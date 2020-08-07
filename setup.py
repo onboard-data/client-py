772fb00c99cca4d6290fa16f41407d98bd8c79c3
Nathan Merritt <nathan.merritt@onboarddata.io>
2020-08-07 14:13:52 -0600

    release v1.2.0 - point selector now supports equipment_type filtering

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.1.1',
+      version='1.2.0',
       author='Nathan Merritt',
       author_email='nathan.merritt@onboarddata.io',
       description='Onboard API SDK',
