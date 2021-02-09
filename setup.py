dbac9654ec1d62c2f8be9bb8b4586317f47cffbc
Nathan Merritt <nathan.merritt@onboarddata.io>
2021-02-09 10:52:52 -0700

    v1.4.0 - more dataframe utils

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.3.4',
+      version='1.4.0',
       author='Nathan Merritt',
       author_email='nathan.merritt@onboarddata.io',
       description='Onboard API SDK',
