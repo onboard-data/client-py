8c8decad3a181effef948a8d98f2375b3581f23e
Nathan Merritt <nathan.merritt@onboarddata.io>
2021-03-15 15:12:45 -0600

    v1.4.3: remove 2019 date boundary on timeseries queries

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.4.2',
+      version='1.4.3',
       author='Nathan Merritt',
       author_email='nathan.merritt@onboarddata.io',
       description='Onboard API SDK',
