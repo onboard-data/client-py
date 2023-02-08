c3f3044daff018144ed6973cd5ca24d02ab5b719
John Vines <john.vines@onboarddata.io>
2023-02-08 11:08:12 -0500

    bumping up patch version for additions to StagingClient constructor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.9.0',
+      version='1.9.1',
       author='Nathan Merritt',
       author_email='nathan.merritt@onboarddata.io',
       description='Onboard API SDK',
