6df1e8bb5a36486fd932e5a097538f2f67ea59c7
John Vines <john.vines@onboarddata.io>
2024-04-18 16:55:39 -0400

    Updating orjson dependency as 3.9.15 fixes a recursion issue

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='onboard.client',
-      version='1.9.4',
+      version='1.9.5',
       author='Nathan Merritt, John Vines',
       author_email='support@onboarddata.io',
       description='Onboard API SDK',
