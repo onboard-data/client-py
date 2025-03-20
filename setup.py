05fd372c1fe0694872831759e89180aef8303850
John Vines <123058+ohshazbot@users.noreply.github.com>
2025-03-20 17:43:17 -0400

    Renaming distribution name to comply with pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setup(name='onboard.client',
+setup(name='onboard_client',
       version='1.14.0',
       author='Nathan Merritt, John Vines',
       author_email='support@onboarddata.io',
